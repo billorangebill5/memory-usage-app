3d4231472992df5d2d916208ddb9aa964e6cbff1
Bill Bronson <161762415+billorangebill5@users.noreply.github.com>
2024-02-29 21:30:58 +0200

    Create memory leaks.js

---
+++ memory leaks.js
@@ -0,0 +1,21 @@
+const connect = require('gulp-connect')
+const autoprefixer = require('gulp-autoprefixer')
+
+
+const babelConfig = {
+    babelHelpers: 'bundled',
+    ignore: ['node_modules'],
+    compact: false,
+    extensions: ['.js', '.html'],
+    plugins: [
+        'transform-html-import-to-string'
+    ],
+    presets: [[
+        '@babel/preset-env',
+        {
+            corejs: 3,
+            useBuiltIns: 'usage',
+            modules: false
+        }
+    ]]
+};
